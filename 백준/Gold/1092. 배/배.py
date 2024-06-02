6938f36f1a22cd678603cbbb7f494036ac871974
Sangwon Woo <riddlewoo713@gmail.com>
2024-06-03 08:14:38 +0900

    [Gold V] Title: 배, Time: 280 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cranes = list(map(int, input().split()))
+
+m = int(input())
+boxes = list(map(int, input().split()))
+
+if max(cranes) < max(boxes):
+    print(-1)
+    sys.exit()
+
+positions = [0] * n
+checked = [False] * m
+cranes.sort(reverse=True)
+boxes.sort(reverse=True)
+
+result, count = 0, 0
+
+while True:
+    if count == len(boxes):
+        break
+    for i in range(n):
+        while positions[i] < len(boxes):
+            if not checked[positions[i]] and cranes[i] >= boxes[positions[i]]:
+                checked[positions[i]] = True
+                positions[i] += 1
+                count += 1
+                break
+            positions[i] += 1
+
+    result += 1
+
+print(result)
