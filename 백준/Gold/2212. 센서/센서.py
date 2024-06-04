d41c3af297d3a7b6d31ca44b3523e382b261cfcd
Sangwon Woo <riddlewoo713@gmail.com>
2024-06-04 19:12:30 +0900

    [Gold V] Title: 센서, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+k = int(input())
+
+if k >= n:
+    print(0)
+    sys.exit()
+
+array = list(map(int, input().split(' ')))
+array.sort()
+
+distances = []
+for i in range(1, n):
+    distances.append(array[i] - array[i-1])
+distances.sort(reverse=True)
+
+for i in range(k-1):
+    distances[i] = 0
+print(sum(distances))
