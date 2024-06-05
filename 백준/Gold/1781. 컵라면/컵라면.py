fac5809c3afe0b2db01b04cf9b8589a42029767f
Sangwon Woo <riddlewoo713@gmail.com>
2024-06-06 07:17:58 +0900

    [Gold II] Title: 컵라면, Time: 520 ms, Memory: 62884 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.py
@@ -0,0 +1,21 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+array, q = [], []
+
+for i in range(n):
+    a, b = map(int, input().split(' ' ))
+    array.append((a,b))
+
+array.sort()
+
+for i in array:
+    a = i[0]
+
+    heapq.heappush(q, i[1])
+    if a < len(q):
+        heapq.heappop(q)
+
+print(sum(q))
