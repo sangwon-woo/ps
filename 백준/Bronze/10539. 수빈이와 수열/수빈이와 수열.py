d8c379b9e9057b411e8a80af4cc738705663450a
Sangwon Woo <riddlewoo713@gmail.com>
2024-06-10 11:10:57 +0900

    [Bronze II] Title: 수빈이와 수열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10539. 수빈이와 수열/수빈이와 수열.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N, B = int(input()), list(map(int, input().split()))
+
+A = [B[0]]
+for i in range(1, N):
+    A.append(B[i] * (i+1) - sum(A))
+
+for i in A:
+    print(i, end=' ')
