90844417876e32d1701914c5b7ce239bae3413ed
Sangwon Woo <riddlewoo713@gmail.com>
2024-08-12 22:44:43 +0900

    [Bronze II] Title: 하얀 칸, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,12 @@
+
+board = [list(input()) for _ in range(8)]
+ans = 0
+for i in range(0,8,2):
+    for j in range(0,8,2):
+        if board[i][j] == 'F':
+            ans += 1
+for i in range(1, 8, 2):
+    for j in range(1, 8, 2):
+        if board[i][j] == 'F':
+            ans += 1
+print(ans)
