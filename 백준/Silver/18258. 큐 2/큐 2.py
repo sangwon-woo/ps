8e477f84ddab102a5a14d1d8335fd3ad946f9c48
Sangwon Woo <riddlewoo713@gmail.com>
2024-08-23 07:03:14 +0900

    [Silver IV] Title: 큐 2, Time: 1376 ms, Memory: 175100 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+N = int(input())
+d = deque([])
+for i in range(N):
+    command = sys.stdin.readline()
+    command = command.split()
+    
+    if command[0] == 'push':
+        d.append(command[1])
+
+    elif command[0] == 'pop':
+        if len(d)==0:
+            print(-1)
+        else:
+            print(d.popleft())
+
+    elif command[0] == 'size':
+        print(len(d))
+
+    elif command[0] == 'empty':
+        if len(d)==0:
+            print(1)
+        else:
+            print(0)
+
+    elif command[0] == 'front':
+        if len(d)==0:
+            print(-1)
+        else:
+            print(d[0])
+
+    elif command[0] == 'back':
+        if len(d)==0:
+            print(-1)
+        else:
+            print(d[-1])
